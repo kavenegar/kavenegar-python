5695ec354cf508200c30b373e4cad1d07b2b7dc3
Alireza Azadi <Alireza_Azadi@Hotmail.com>
2020-10-26 08:31:17 +0330

    setting `python 3.6` as minimum version

--- setup.py
+++ setup.py
@@ -2,20 +2,20 @@ from setuptools import setup
 import sys
 
 requires = ['requests>=0.10.8']
-if sys.version_info < (2, 6):
-    requires.append('simplejson')
+if sys.version_info < (3, 6):
+    sys.exit('Python < 3.6 is not supported')
 
 setup(
-    name = "kavenegar",
-    py_modules = ['kavenegar'],
-    version = "1.1.3",
-    description = "Kavenegar Python library",
-    author = "Kavenegar Team",
-    author_email = "support@kavenegar.com",
-    url = "https://github.com/kavenegar/kavenegar-python",
-    keywords = ["kavenegar", "sms"],
-    install_requires = requires,
-    classifiers = [
+    name="kavenegar",
+    py_modules=['kavenegar'],
+    version="1.1.3",
+    description="Kavenegar Python library",
+    author="Kavenegar Team",
+    author_email="support@kavenegar.com",
+    url="https://github.com/kavenegar/kavenegar-python",
+    keywords=["kavenegar", "sms"],
+    install_requires=requires,
+    classifiers=[
         "Programming Language :: Python",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: MIT License",
@@ -23,5 +23,5 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Communications :: Telephony",
-        ]
-     )
+    ]
+)
